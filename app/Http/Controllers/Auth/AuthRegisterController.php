bc995c4716b5a0c9af0ea542362c4e1e022211ec
codelord <richarddaaki4@gmail.com>
2024-09-22 20:18:47 +0300

    new - initial auth setup

---
+++ app/Http/Controllers/Auth/AuthRegisterController.php
@@ -0,0 +1,45 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Http\Controllers\Auth;
+
+
+use App\Http\Requests\Auth\AuthRegisterRequest;
+use App\Models\Company;
+use App\Models\Freelancer;
+use App\Models\User;
+use Illuminate\Contracts\View\View;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Routing\Controller;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+
+use Illuminate\Validation\ValidationException;
+use Symfony\Component\HttpFoundation\Response;
+
+final class AuthRegisterController extends Controller
+{
+    public function __invoke(AuthRegisterRequest $request): View
+    {
+        $data = $request->validated();
+
+        $is_company = $request->is_company ?? 0;
+
+        $user =   User::create([
+            'name' => $request->full_name,
+            'email' => $request->email,
+            'password' => password_hash($request->password, PASSWORD_DEFAULT),
+            'userable_type' => $is_company ? Company::class : Freelancer::class,
+            'userable_id' => 0,
+        ]);
+
+        Auth::login($user);
+
+        if ($is_company) {
+            return view('onboard-screen-employer');
+        } else {
+            return redirect()->to(route('onboard-screen'));
+        }
+    }
+}
